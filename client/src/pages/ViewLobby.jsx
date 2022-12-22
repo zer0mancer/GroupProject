62e432560b62a57d1b70917375283a9c86ccd5c3
connor s maloney <maloney3.cs@gmail.com>
2022-12-22 13:50:52 -0700

    lobby view is done

--- client/src/pages/ViewLobby.jsx
+++ client/src/pages/ViewLobby.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
 
 
-const ViewLobby = ({ lobby }) => {
+const ViewLobby = () => {
 
     const [ userId, setUserId ] = useState(localStorage.getItem('userId'));
     const [ accessToken, setAccessToken ] = useState(localStorage.getItem('accessToken'));
@@ -12,26 +11,24 @@ const ViewLobby = ({ lobby }) => {
     const [ title, setTitle ] = useState('');
     const [ platform, setPlatform ] = useState('');
     const [ creatorId, setCreatorId ] = useState('');
+    const [ game, setGame ] = useState('');
+    const [ limit, setLimit ] = useState('');
 
 
-    useEffect(()=>{
-        axios.get(
-            `http://localhost:8000/api/users/${id}`,
-            {headers:
-            { "Authorization": `Bearer ${accessToken}`}
-            },
-            {withCredentials: true})
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/lobbies/${id}`)
             .then((response)=>{
-                console.log(response.data.user);
-                setUsername(response.data.user.username);
-                setEmail(response.data.user.email);
-                setUserId(response.data.user._id);
-                // setPlatform(response.data.user.platform);
+                console.log(response.data.server, "server response");
+                setTitle(response.data.server.title);
+                setPlatform(response.data.server.platform);
+                setCreatorId(response.data.server.creatorId);
+                setGame(response.data.server.game);
+                setLimit(response.data.server.limit);
             })
             .catch((error)=>{
                 console.log(error)
             })
-    },[])
+    },[]);
 
 
 
@@ -39,12 +36,13 @@ const ViewLobby = ({ lobby }) => {
   return (
     <div className='mb-44 absolute top-[103px] text-white right-10 z-100 bg-slate-700 shadow rounded p-4'>
       <div className='flex flex-col items-center'>
-      <h1 className="text-2xl m-3">{lobby.title}</h1>
+      <h1 className="text-2xl m-3">{title}</h1>
         <div className='flex'>
-          <h3 className='text-xl m-3'>Game: {lobby.game}</h3>
-          <h3 className='text-xl m-3'>Platform: {lobby.platform}</h3>
-          <h3 className='text-xl m-3'>Player Limit: {lobby.limit}</h3>
-          // use populate function on back end to display users name
+          <h1>{id}</h1>
+          <h3 className='text-xl m-3 text-red-500'>Game: {game}</h3>
+          <h3 className='text-xl m-3'>Platform: {platform}</h3>
+          <h3 className='text-xl m-3'>Player Limit: {limit}</h3>
+
         </div>
         <button className="border border-white font-bold rounded p-2 m-2 bg-slate-700 hover:bg-white hover:text-slate-700 text-white">Return</button>
       </div>
